a98fedc3d7096dadeb98d3cde671380a389a9729
theroncross <theroncross@gmail.com>
2016-09-27 12:59:13 -0500

    format time display and add labels and tests to stopwatch

--- src/timer/stopwatch.test.js
+++ src/timer/stopwatch.test.js
@@ -1,1 +1,27 @@
-timer.test.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import ReactTestUtils from 'react-addons-test-utils';
+import Stopwatch from './stopwatch';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Stopwatch />, div);
+});
+
+it('displays "START" when not running', () => {
+  const sw = shallow( <Stopwatch /> );
+  expect(sw.text()).toContain('START');
+});
+
+it('displays "LAP" when running before all runners have finished', () => {
+  const sw = shallow( <Stopwatch allRunnersFinished={false} /> );
+  sw.find('.stopwatch').simulate('click');
+  expect(sw.text()).toContain('LAP');
+});
+
+it('displays "STOP" when running before all runners have finished', () => {
+  const sw = shallow( <Stopwatch allRunnersFinished={true} /> );
+  sw.find('.stopwatch').simulate('click');
+  expect(sw.text()).toContain('STOP');
+});
